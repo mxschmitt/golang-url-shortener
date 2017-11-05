738220d43d706524faf2137e31c58472cfe9c746
Max Schmitt <max@schmitt.mx>
2017-11-05 22:18:38 +0100

    Moved to react-router 1/2

--- static/src/index.js
+++ static/src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import 'semantic-ui-css/semantic.min.css';
 import App from './App/App';
-import Menu from './Menu/Menu';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Menu />, document.getElementById('root'));
+import { BrowserRouter } from 'react-router-dom'
+
+ReactDOM.render((
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+), document.getElementById('root'))
 registerServiceWorker();
