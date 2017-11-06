067763e9976c03b2d0ffadfb92c0f5455f017cac
Max Schmitt <max@schmitt.mx>
2017-11-06 14:53:05 +0100

    Added graphical menu

--- static/src/Home/Home.js
+++ static/src/Home/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Container, Input, Segment, Form, Modal, Button } from 'semantic-ui-react'
+import { Input, Segment, Form, Modal, Button } from 'semantic-ui-react'
 
 import './Home.css';
 
@@ -75,16 +75,14 @@ export default class HomeComponent extends Component {
     const { open, authorized } = this.state
     if (authorized) {
       return (
-        <Container id='rootContainer' >
-          <Segment raised>
-            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.</p>
-            <Form onSubmit={this.handleURLSubmit}>
-              <Form.Field>
-                <Input size='big' action={{ icon: 'arrow right', labelPosition: 'right', content: 'Shorten' }} type='url' onChange={this.handleURLChange} name='url' placeholder='Paste a link to shorten it' />
-              </Form.Field>
-            </Form>
-          </Segment>
-        </Container>
+        <Segment raised>
+          <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.</p>
+          <Form onSubmit={this.handleURLSubmit}>
+            <Form.Field>
+              <Input size='big' action={{ icon: 'arrow right', labelPosition: 'right', content: 'Shorten' }} type='url' onChange={this.handleURLChange} name='url' placeholder='Paste a link to shorten it' />
+            </Form.Field>
+          </Form>
+        </Segment>
       )
     } else {
       return (
