0f9903952a9d6f701f6aecaa6a06be95d056be13
Max Schmitt <max@schmitt.mx>
2017-11-06 14:22:02 +0100

    - Added routing

--- static/src/index.js
+++ static/src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import 'semantic-ui-css/semantic.min.css';
-import App from './App/App';
 import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom';
+import {
+    HashRouter,
+    Route
+} from 'react-router-dom'
 
-import { BrowserRouter } from 'react-router-dom'
+import Header from './Header/Header'
+import About from './About/About'
+import Home from './Home/Home'
+
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render((
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <HashRouter>
+        <div>
+            <Header />
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+        </div>
+    </HashRouter>
 ), document.getElementById('root'))
+
 registerServiceWorker();
