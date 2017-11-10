8f923a0d841cc0884f1646893247c2a00aec78ae
Max Schmitt <max.andreas.schmitt@googlemail.com>
2017-11-10 01:04:24 +0100

    Fixed incorrect permissions of the schema.json

--- build/schema.go
+++ build/schema.go
@@ -14,7 +14,7 @@ func main() {
 	schemaPath := flag.String("path", filepath.Join("build", "schema.json"), "location of the converted schema")
 	flag.Parse()
 	schema := generator.Generate(&config.Configuration{})
-	if err := ioutil.WriteFile(*schemaPath, []byte(schema), 644); err != nil {
+	if err := ioutil.WriteFile(*schemaPath, []byte(schema), 755); err != nil {
 		log.Fatalf("could not write schema: %v", err)
 	}
 }
