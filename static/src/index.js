e6ec8df3a6724f20c881578936f784ef8158dc43
Max Schmitt <max@schmitt.mx>
2017-11-12 16:19:19 +0100

    fixed 100%+15px height bug of site

--- static/src/index.js
+++ static/src/index.js
@@ -96,7 +96,7 @@ export default class BaseComponent extends Component {
         }
         return (
             <HashRouter>
-                <Container style={{ marginTop: "15px" }}>
+                <Container style={{ paddingTop: "15px" }}>
                     <Menu stackable>
                         <Menu.Item as={Link} to="/" name='shorten' onClick={this.handleItemClick} >
                             <Image src={userData.Picture} alt='user profile' circular size='mini' />
